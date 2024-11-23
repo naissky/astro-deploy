55cec60050aea0fbf386b5c98991cf47d353ed8c
naissky <outdaywork@gmail.com>
2024-11-23 10:12:05 -0500

    Server => static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel()
 });
